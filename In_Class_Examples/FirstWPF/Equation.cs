ce92ea79f5d765da22746770958e6961f6339b15
Ackerman, Adam P <adam@ou.edu>
2023-08-24 10:07:25 -0500

    Started a new solution

---
+++ In_Class_Examples/FirstWPF/Equation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FirstWPF
+{
+    public class Equation
+    {
+        public double Left { get; set; }
+
+        public double Right { get; set; }
+
+        public string name { get; set; }
+
+        /// <summary>
+        /// Empty/default constructor
+        /// </summary>
+        public Equation()
+        {
+            Left = 0;
+            Right = 0;
+            name = string.Empty;
+        }
+
+        /// <summary>
+        /// Overloaded constructor
+        /// </summary>
+        /// <param name="left">Left operand</param>
+        /// <param name="right">Right Operand</param>
+        public Equation(double left, double right)
+        {
+            Left = left;
+            Right = right;
+            name = string.Empty;
+            name = "";
+        }
+
+        public double Add()
+        {
+            double answer = 0;
+
+            answer = Left + Right;
+            return answer;
+        }
+
+        public double Division()
+        {
+            double answer = 0;
+
+            if (Right != 0)
+            {
+                answer = Left / Right; 
+            }
+            else
+            {
+                answer = -99999;
+            }
+
+            return answer;
+        }
+
+    }
+}
---
+++ In_Class_Examples/FirstWPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //MessageBox.Show("Hello, the application is now loaded!");  
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string userInput = txtFirstNumber.Text;
+
+            userInput = userInput.Replace(" ", "");
+
+            double num1;
+            
+            if (double.TryParse(userInput, out num1) == false)
+            {
+                MessageBox.Show($"Sorry, {userInput} is not a number");
+                return;
+            }
+            //MessageBox.Show(num1.ToString("C"));
+            lblOutput.Content = num1.ToString("C");
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            double left = Convert.ToDouble(txtFirstNumber.Text);
+            double right = Convert.ToDouble(txtSecondNumber.Text);
+
+            Equation problem1 = new Equation();
+            problem1.Left = left;
+            problem1.Right = right; 
+
+            double sum = problem1.Add();
+            double quotient = problem1.Division();
+
+            MessageBox.Show($"The sum is {sum} and the quotient is {quotient}");
+
+            
+        }
+    }
+}
