a3868d0d776ca607d4ef5a50c4aa21fd569495b1
Ackerman, Adam P <adam@ou.edu>
2023-08-31 09:12:29 -0500

    Completed the Student class and the GUI

--- Participations/FirstWPFApplication/MainWindow.xaml.cs
+++ Participations/FirstWPFApplication/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace FirstWPFApplication
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
     }
 }
---
+++ Participations/FirstWPFApplication/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstWPFApplication
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+        public DateTime? Birthdate { get; set; }
+
+        /// <summary>
+        /// Default empty constructor
+        /// </summary>
+        public Student()
+        {
+            Name =  string.Empty;
+            Birthdate = null;
+        }
+
+        /// <summary>
+        /// Calculates the age of the student
+        /// </summary>
+        /// <returns>The age of the student as a whole number</returns>
+        public int CalculateAge()
+        {
+            if (Birthdate.HasValue)
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - Birthdate.Value.Year;
+                if (Birthdate.Value > today.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+    }
+}
