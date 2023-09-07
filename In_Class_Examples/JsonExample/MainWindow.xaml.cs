731754b4f4701909dfbf24b6f78fea53ae407434
Ackerman, Adam P <adam@ou.edu>
2023-09-07 09:53:14 -0500

    Completed an in class example over JSON

---
+++ In_Class_Examples/JsonExample/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JsonExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string fileContentsAsJson = File.ReadAllText("MOCK_DATA.json");
+            /*
+             [ 
+                {
+                  "id": 1,
+                  "first_name": "Twyla",
+                  "last_name": "Dykins",
+                  "email": "tdykins0@paginegialle.it",
+                  "gender": "Female",
+                  "ip_address": "139.162.154.22",
+                  "city": "Tshikapa"
+                },
+            */
+            List<Person> people = JsonConvert.DeserializeObject<List<Person>>(fileContentsAsJson) ;
+
+            foreach (Person person in people)
+            {
+                lstPeople.Items.Add(person);
+            }
+
+        }
+
+        private void lstPeople_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Person selectedPerson = (Person)lstPeople.SelectedItem;
+
+            MessageBox.Show($"{selectedPerson.email} , {selectedPerson.gender}");
+        }
+    }
+}
---
+++ In_Class_Examples/JsonExample/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonExample
+{
+    public class Person
+    {
+        public int id { get; set; }
+
+        public string first_name { get; set; }
+
+        public string last_name { get; set; }
+
+        public string email { get; set; }
+        public string gender { get; set; }
+
+        // skip ip_address
+
+        public string city { get; set; }
+
+        public Person()
+        {
+            // empty constructor
+            first_name = string.Empty;
+            last_name = string.Empty;
+            email = string.Empty;
+            gender = string.Empty;
+            city = string.Empty;
+        }
+
+        public override string ToString()
+        {
+            return $"{first_name} {last_name} {city}";
+        }
+    }
+}
