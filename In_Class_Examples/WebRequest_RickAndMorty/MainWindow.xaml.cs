e722ad4ae489ed45bff392db456406cea130066f
Ackerman, Adam P <adam@ou.edu>
2023-09-18 14:49:53 -0500

    Did in class example over rick and morty

---
+++ In_Class_Examples/WebRequest_RickAndMorty/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WebRequest_RickAndMorty
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string url = @"https://rickandmortyapi.com/api/character";
+
+
+            HttpClient client = new HttpClient();
+
+            string json = client.GetStringAsync(url).Result;
+
+            RickAndMortyAPI api;
+
+            api = JsonConvert.DeserializeObject<RickAndMortyAPI>(json);
+
+            foreach (Result character in api.results)
+            {
+                cboCharacters.Items.Add(character);
+            }
+         
+        }
+
+        private void cboCharacters_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCharacter = (Result) cboCharacters.SelectedItem;
+
+            txtName.Text = selectedCharacter.name;
+
+            imgCharacter.Source = new BitmapImage(new Uri(selectedCharacter.image));
+        }
+    }
+}
