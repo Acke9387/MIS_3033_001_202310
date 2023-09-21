0069b966a3613c93c453f160a1a8d6b2dac28f5d
Ackerman, Adam P <adam@ou.edu>
2023-09-21 09:36:55 -0500

    started pokemon participation

---
+++ Participations/Json_Pokemon/PokemonAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json_Pokemon
+{
+    public class PokemonAPI
+    {
+        public List<PokemonItem> results { get; set; }
+    }
+
+    public class PokemonItem
+    {
+
+        public string name { get; set; }
+
+        public string url { get; set; }
+
+
+    }
+}
